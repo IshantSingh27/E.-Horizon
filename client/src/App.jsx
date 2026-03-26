3eadc76f05a7c2471f7e49570419b919b1271744
IshantSingh27 <ishantsingh0602@gmail.com>
2026-03-26 10:18:28 +0530

    Payment Success

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-// import EventDetail from './pages/EventDetail';
+import EventDetail from './pages/EventDetail';
 import Login from './pages/Login';
-// import Register from './pages/Register';
+import Register from './pages/Register';
 // import UserDashboard from './pages/UserDashboard';
 // import AdminDashboard from './pages/AdminDashboard';
 // import PaymentSuccess from './pages/PaymentSuccess';
@@ -18,9 +18,9 @@ function App() {
             <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    {/* <Route path="/events/:id" element={<EventDetail />} /> */}
+                    <Route path="/events/:id" element={<EventDetail />} />
                     <Route path="/login" element={<Login />} />
-                    {/* <Route path="/register" element={<Register />} /> */}
+                    <Route path="/register" element={<Register />} />
                     {/* <Route path="/dashboard" element={<UserDashboard />} /> */}
                     {/* <Route path="/admin" element={<AdminDashboard />} /> */}
                     {/* <Route path="/payment-success" element={<PaymentSuccess />} /> */}
